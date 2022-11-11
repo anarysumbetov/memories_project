abd7a9620b4386d9e7491101f84d80d2d8fc6246
Anarys Umbetov <brodiaga_sirim@mail.ru>
2022-11-11 21:20:22 +0600

    index.js mongodb connection

--- server/index.js
+++ server/index.js
@@ -9,4 +9,11 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://it-superninja:it-superninja123@cluster0.miovexf.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
 // https://www.mongodb.com/
