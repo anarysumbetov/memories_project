ec522432762266f08fe8e93b24c7cdae95c620b2
Anarys Umbetov <brodiaga_sirim@mail.ru>
2023-01-04 21:08:43 +0600

    postdetails and paginate components created

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import { Container } from "@material-ui/core";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+import PostDetails from "./components/PostDetails/PostDetails.jsx";
 import Navbar from "./components/Navbar/Navbar.js";
 import Home from "./components/Home/Home.js";
 import Auth from "./components/Auth/Auth.js";
@@ -10,10 +11,13 @@ function App() {
   return (
     <GoogleOAuthProvider clientId="372569767609-k3dgt3ttvnus7lj7ke95uga6197m85r3.apps.googleusercontent.com">
       <BrowserRouter>
-        <Container maxWidth="lg">
+        <Container maxWidth="xl">
           <Navbar />
           <Switch>
-            <Route path="/" exact component={Home} />
+            <Route path="/" exact component={() => <Redirect to="/posts" />} />
+            <Route path="/posts" exact component={Home} />
+            <Route path="/posts/search" exact component={Home} />
+            <Route path="/posts/:id" exact component={PostDetails} />
             <Route path="/auth" exact component={Auth} />
           </Switch>
         </Container>
