9522de7d2aeb607315cfc6381f19db7d9df5639a
Anarys Umbetov <brodiaga_sirim@mail.ru>
2023-01-07 20:35:06 +0600

    fetchbysearch added

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,8 +1,10 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from "../constants/actionTypes.js";
+import { FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE } from "../constants/actionTypes.js";
 
 const posts = (posts = [], action) => {
     switch (action.type) {
         case FETCH_ALL:
+            return action.payload;
+        case FETCH_BY_SEARCH:
             return action.payload;
         case LIKE:
             return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
