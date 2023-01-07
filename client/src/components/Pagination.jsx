670dc9bcb65ec26b075fc1509c9345b557b00171
Anarys Umbetov <brodiaga_sirim@mail.ru>
2023-01-07 22:05:07 +0600

    paginate component changed

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -8,6 +8,7 @@ import { getPosts } from "../actions/posts.js";
 import useStyles from "./styles.js";
 
 const Paginate = ({ page }) => {
+    const { numberOfPages } = useSelector((state) => state.posts);
     const dispatch = useDispatch();
 
     const classes = useStyles();
@@ -21,12 +22,12 @@ const Paginate = ({ page }) => {
     return (
         <Pagination 
             classes={{ ul: classes.ul }}
-            count={5}
-            page={1}
+            count={numberOfPages}
+            page={Number(page) || 1}
             variant="outlined"
             color="primary"
             renderItem={(item) => (
-                <PaginationItem {...item} component={Link} to={`/posts?page=${1}`} />
+                <PaginationItem {...item} component={Link} to={`/posts?page=${item.page}`} />
             )}
         />
     );
