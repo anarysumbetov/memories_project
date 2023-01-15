b5429c43d19f781801d5ed0d939b685fcf2d3fd4
Anarys Umbetov <brodiaga_sirim@mail.ru>
2023-01-15 21:22:36 +0600

    Comment Section changed

--- client/src/components/PostDetails/CommentSection.jsx
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { Typography, TextField, Button } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
-const CommentSection = () => {
+import useStyles from "./styles.js";
+
+const CommentSection = ({ post }) => {
+  console.log("CommentSection post:", post);
+  
+  const [comment, setComment] = useState('');
+  const dispatch = useDispatch();
+  const [comments, setComments] = useState([1, 2, 3, 4]);
+  const classes = useStyles();
+  const commentsRef = useRef();
+
+  const handleComment = async () => {
+
+  };
+
   return (
-    <div>CommentSection</div>
+    <div>
+      <div className={classes.commentsOuterContainer}>
+        <div className={classes.commentsInnerContainer}>
+          <Typography gutterBottom variant="h6">Comments</Typography>
+          {comments?.map((c, i) => (
+            <Typography key={i} gutterBottom variant="subtitle1">
+              Comment {i}
+            </Typography>
+          ))}
+        </div>
+        <div style={{ width: '70%' }}>
+          <Typography gutterBottom variant="h6">Write a comment</Typography>
+          <TextField 
+            fullWidth
+            minRows={4}
+            variant="outlined"
+            label="Comment"
+            multiline
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <br />
+          <Button 
+            style={{ marginTop: '10px' }}
+            fullWidth
+            disabled={!comment.length}
+            color="primary"
+            variant="contained"
+            onClick={handleComment}
+          >
+            Comment
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -52,7 +52,7 @@ const PostDetails = () => {
                     <Divider style={{ margin: '20px 0' }} />
                     <Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>
                     <Divider style={{ margin: '20px 0' }} />
-                    <Typography variant="body1"><strong>Comments - coming soon!</strong></Typography>
+                    <CommentSection post={post} />
                     <Divider style={{ margin: '20px 0' }} />
                 </div>
                 <div className={classes.imageSection}>
